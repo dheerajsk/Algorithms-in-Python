ce9d3bad4faaa2d25e918bac8563b2bc4a0f87d8
Dheeraj Kumar <kumardh@transitioncomputing.com>
2019-11-16 21:55:18 +0530

    Selection sort time complexity

--- Sorting/selection-sort.py
+++ Sorting/selection-sort.py
@@ -1,7 +1,7 @@
 def selectionSort(array):
     for i in range(len(array)-1):
         minIndex = i
-        for j in range(i+1, len(array)):
+        for j in range(i+1, len(array)): 
             if array[j]<array[minIndex]:
                 minIndex=j
         if i!=minIndex:
@@ -13,3 +13,5 @@ def selectionSort(array):
 inputArray = [16,33,84,15,9,68]
 selectionSort(inputArray)
 print(inputArray)
+
+# T(n) = O(n2) 
